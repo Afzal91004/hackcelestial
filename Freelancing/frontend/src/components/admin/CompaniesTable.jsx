958afa60ac6d09725e893151eee449d693d36aa9
kshxaib <khanshoaibishtiytak@gmail.com>
2024-09-17 13:58:04 +0530

    improvement

--- Freelancing/frontend/src/components/admin/CompaniesTable.jsx
+++ Freelancing/frontend/src/components/admin/CompaniesTable.jsx
@@ -1,20 +1,26 @@
-import React from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../ui/table";
+import React, { useEffect, useState } from "react";
+import {Table,TableBody,TableCaption,TableCell,TableHead,TableHeader,TableRow} from "../ui/table";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Edit2, MoreHorizontal } from "lucide-react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const CompaniesTable = () => {
-  const { companies } = useSelector((store) => store.company);
+  const { companies, searchCompanyByText} = useSelector((store) => store.company);
+  const [filterCompany, setFfilterCompany] = useState(companies);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+     const filteredCompany = companies.length >= 0 && companies.filter((company)=>{
+      if(!searchCompanyByText) {
+        return true
+      };
+      return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+
+     });
+     setFfilterCompany(filteredCompany);
+  },[companies, searchCompanyByText])
   return (
     <div>
       <Table>
@@ -28,7 +34,8 @@ const CompaniesTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {companies?.map((company, index) => (
+          {
+          filterCompany?.map((company, index) => (
             <tr key={index}>
               <TableCell>
                 <Avatar>
@@ -43,7 +50,7 @@ const CompaniesTable = () => {
                     <MoreHorizontal />
                   </PopoverTrigger>
                   <PopoverContent className="w-32">
-                    <div className="flex items-center gap-2 w-fit cursor-pointer">
+                    <div onClick={() => navigate(`/admin/companies/${company._id}`)} className="flex items-center gap-2 w-fit cursor-pointer">
                       <Edit2 className="w-4" />
                       <span>Edit</span>
                     </div>
