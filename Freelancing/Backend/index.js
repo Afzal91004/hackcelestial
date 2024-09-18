a4f1c91a099673290e33e6593f64b589382d818a
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2024-09-18 21:52:04 +0530

    fixes

--- Freelancing/Backend/index.js
+++ Freelancing/Backend/index.js
@@ -18,10 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5174", "http://localhost:5173"],
   credentials: true,
 };
-
 
 app.use(cors(corsOptions));
 
