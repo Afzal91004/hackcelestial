00489fff75a3c7d1f4e8edc89cc07b021380ba8a
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2024-09-18 15:53:17 +0530

    fixes

--- Freelancing/Backend/index.js
+++ Freelancing/Backend/index.js
@@ -1,3 +1,4 @@
+// index.js
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -14,28 +15,28 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173','http://localhost:5174','https://talent-tribe-backend.vercel.app/'],
-    credentials: true,
-}
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://talent-tribe-backend.vercel.app/",
+  ],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-
-// api's 
+// api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
- 
- 
 
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
